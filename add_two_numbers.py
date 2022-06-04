1f407b51d77ebacbd1f4e0f509b70f2676752e21
Andrew Farrell <farrellas05@yahoo.com>
2022-06-03 19:01:44 -0600

    updated solutions

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -4,34 +4,33 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
-    def addTwoNumbers(self, l1, l2):
-        num1 = self.findListNum(l1)
-        num2 = self.findListNum(l2)
-        total = num1 + num2
-        return self.convertToLL(total)
-    
-    def findListNum(self, ll):
-        output = ''
-        while ll.next:
-            output += str(ll.val)
-            ll = ll.next
+def addTwoNumbers(self, l1, l2):
+    num1 = self.findListNum(l1)
+    num2 = self.findListNum(l2)
+    total = num1 + num2
+    return self.convertToLL(total)
+
+def findListNum(self, ll):
+    output = ''
+    while ll.next:
         output += str(ll.val)
-        output = output[::-1]
-        return int(output)
-    
-    def convertToLL(self, input):
-        ll = []
-        str_input = str(input)
-        i = len(str_input) - 1
-        while i >= 0:
-            if i == 0:
-                value = int(str_input[i])
-                ll.append(ListNode(value))
-            else:
-                value = int(str_input[i])
-                next_node = ll
-                ll.append(ListNode(value, next_node))
-            i -= 1
-        return ll
-        
+        ll = ll.next
+    output += str(ll.val)
+    output = output[::-1]
+    return int(output)
+
+def convertToLL(self, input):
+    ll = []
+    str_input = str(input)
+    i = len(str_input) - 1
+    while i >= 0:
+        if i == 0:
+            value = int(str_input[i])
+            ll.append(ListNode(value))
+        else:
+            value = int(str_input[i])
+            next_node = ll
+            ll.append(ListNode(value, next_node))
+        i -= 1
+    return ll
+
--- longest_substring.py
+++ longest_substring.py
@@ -1,29 +1,28 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) < 2:
-            return len(s)
-        maximum = 1
-        letters = set()
-        l = 0
-        r = 1
-        letters.add(s[l])
-        while r < len(s):
-            print(l, r, letters)
-            if len(letters) < maximum:
-                l += 1
-                r += 1
-                letters = set(s[l:r+1])
+def lengthOfLongestSubstring(self, s: str) -> int:
+    if len(s) < 2:
+        return len(s)
+    maximum = 1
+    letters = set()
+    l = 0
+    r = 1
+    letters.add(s[l])
+    while r < len(s):
+        print(l, r, letters)
+        if len(letters) < maximum:
+            l += 1
+            r += 1
+            letters = set(s[l:r+1])
+            
+        elif s[r] in letters:
+            maximum = max(len(letters), maximum)
+            l += 1
+            r = l + maximum + 1
+            letters = set(s[l:r])
                 
-            elif s[r] in letters:
-                maximum = max(len(letters), maximum)
-                l += 1
-                r = l + maximum + 1
-                letters = set(s[l:r])
-                    
-            else:
-                letters.add(s[r])
-                r += 1
-                
-        maximum = max(len(letters), maximum)
-        
-        return maximum
+        else:
+            letters.add(s[r])
+            r += 1
+            
+    maximum = max(len(letters), maximum)
+    
+    return maximum
