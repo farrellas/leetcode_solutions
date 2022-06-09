0396f3cf03b3e59ad098696c95ac11e02ec18c42
Andrew Farrell <farrellas05@yahoo.com>
2022-06-08 21:23:42 -0600

    updated solutions

--- implement_queue.py
+++ implement_queue.py
@@ -1,15 +1,18 @@
-import collections
-
 class MyQueue:
 
     def __init__(self):
-        self.q = collections.deque()
+        self.q = []
 
     def push(self, x: int) -> None:
         self.q.append(x)
 
     def pop(self) -> int:
-        return self.q.popleft()
+        output = self.q[0]
+        if len(self.q) > 1:
+            self.q = self.q[1:]
+        else:
+            self.q = []
+        return output
 
     def peek(self) -> int:
         return self.q[0]
